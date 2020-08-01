80c603e64ebc305f151dbc01c82393ba2b7cb078
mia.hasnutdinova@gmail.com <mia.hasnutdinova@gmail.com>
2020-08-01 12:34:30 -0400

    add happyAgain.java

---
+++ gitDemo/src/HappyB20.java
@@ -0,0 +1,19 @@
+public class HappyB20 {
+
+    public static void main(String[] args) {
+
+// find frequency of a word froms string Array
+
+            String[] str = {"RUmCake", "Java", "Cake", "Cake"};
+            String findWord = "cake";
+            int count = 0;
+            for (String each : str) {
+                if (findWord.equalsIgnoreCase(each)) {
+                    count++;
+                }
+            }
+            System.out.println(findWord + " " +count);
+            System.out.println( "hello hello");
+            System.out.println( "hello hello");
+    }
+}
